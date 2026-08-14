03866ac04d3e56cc4b5bd811e7fcf8fa391d705f
riad ben10011 <riadbendraoua@gmail.com>
2026-08-14 19:55:24 +0100

    Add files via upload

---
+++ actlis.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class actlis implements ActionListener {
+	
+		protected JButton b1,b2;
+                protected JLabel l;
+	public void act () {
+	JFrame f = new JFrame ("act");
+	JPanel p1 = new JPanel ();
+	 JPanel p2 = new JPanel ();
+	/*panel 1*/
+	l=new JLabel ("choose one ");
+	p1.setLayout(new BorderLayout ());
+	p1.add(l);
+	//panel 2
+	b1=new JButton ("spider man brand new day ");
+	b2=new JButton ("avengers doomsday");
+	b1.setBackground (new Color (255,1,1));
+	b2.setBackground (new Color (1,255,1));
+	p2.add(b1);
+	p2.add(b2);
+	f.add(p1,BorderLayout.NORTH);
+	f.add(p2,BorderLayout.CENTER);
+	b1.addActionListener(this);
+	b2.addActionListener(this);
+	f.setSize(800,400);
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       	f.setVisible(true);
+	}	
+	public static void main (String [] args ){
+	actlis a = new actlis ();
+	a.act ();
+	}
+	@Override 
+       public void actionPerformed (ActionEvent e){
+       if (e.getSource() == b1 ){
+       l.setText("watched");
+       } else {
+       l.setText("in december");
+       }
+       }	
+}
