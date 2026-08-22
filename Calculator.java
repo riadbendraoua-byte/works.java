b7eb3d2b94b65f55b0a8fca64ace35997d689fec
riadbendraoua-byte <riadbendraoua@gmail.com>
2026-08-22 17:15:00 +0100

    learning

--- Calculator.java
+++ Calculator.java
@@ -272,3 +272,19 @@ public class Calculator implements ActionListener {
 		}
 	
 	}}
+const xArray = ["Italy","France","Spain","USA","Argentina"];
+const yArray = [55, 49, 44, 24, 15];
+
+const data = [{
+  x: xArray,
+  y: yArray,
+  type: "bar",
+  orientation:"v",
+  marker: {color:"rgba(0,0,255)"}
+}];
+
+const layout = {title:"World Wide Wine Production"};
+
+Plotly.newPlot("myPlot", data, layout);
+
+
